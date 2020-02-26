156571a7556d3262b506b59b0506e8026ba4a311
Sergio Escalante <sergioescalante@ibm.com>
2020-02-26 14:29:45 -0500

    Delete history files

--- .history/api_call_20200224162615.py
+++
@@ -1,8 +0,0 @@
-import json
-import requests
-
-api_token = 'mytokenhere'
-api_url = 'https://opaas.amm.ibmcloud.com/api/v1/instances/'
-
-headers = {'Content-Type': 'application/json',
-           'Authorization': 'Bearer {0}'.format(api_token)}
